f9a6e980a81ec6928b34a5e5624d39b8ca3d616d
alWestDeden <baboumediene@laposte.net>
2023-08-10 16:50:46 +0200

    homepage, links & 404

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import Home from './pages/Home'
 // import Apartment from './pages/Apartment'
-// import Error from './pages/Error'
+import Error from './pages/Error'
 import Infos from './pages/Infos'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,7 +16,7 @@ ReactDOM.render(
         <Route path="/" element={<Home />} />
         {/* <Route path="/apartment/:id" element={<Apartment />} /> */}
         <Route path="/infos" element={<Infos />} />
-        {/* <Route path="*" element={<Error />} /> */}
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import '../style/error.scss'
+
+function Error() {
+  return (
+    <div className="error">
+      <p className="code">404</p>
+      <p className="alert">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retournez sur la page d'acceuil</Link>
+    </div>
+  )
+}
+export default Error
